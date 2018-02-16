c599b00e1771371965cf99f264c1a52d4f440c30
MohamedAhmedShalata <36453658+MohamedAhmedShalata@users.noreply.github.com>
2018-02-16 21:57:15 +0200

    AVR learning example
    flashing LED example with microprocessor AVR using codeblocks as an IDE and protues for simulation

---
+++ LED/main.c
@@ -0,0 +1,22 @@
+/*
+ */
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+    DDRA=0b00000001;
+
+
+    while(1) {
+        PORTA=0b00000001;
+       _delay_ms(500);
+        PORTA=0b00000000;
+       _delay_ms(500);
+    }
+
+
+    return 0;
+}
