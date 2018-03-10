2d17e69280ee981b5466e87719c375bf09ffac39
MohamedAhmedShalata <36453658+MohamedAhmedShalata@users.noreply.github.com>
2018-03-11 00:07:14 +0200

    Add files via upload

---
+++ sensor interupt demo.h
@@ -0,0 +1,23 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void INIT_INTERUPT(){
+
+    DDRD &=~(1<<PD2);
+    PORTD|=(1<<PD2);
+    MCUCR|=(1<<ISC01);
+    GICR|=(1<<INT0);
+
+    sei();
+}
+//not sure this will work
+void run_interupt(){
+
+    ISR(INT0_vect){
+
+       PORTA =0x00;
+    }
+
+}
