28b6d18ece5cd27673928c72fde77132a4c6561f
MohamedHamedOo <36466919+MohamedHamedOo@users.noreply.github.com>
2018-03-13 22:51:54 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,54 @@
+/*
+ * ServoMotor.c
+ *
+ * Created: 10/03/2018 11:10:58 م
+ * Author : MohameD AhmeD
+ */ 
 
-#define F_CPU 1000000UL
-#include <avr/io.h>
-#include <util/delay.h>
+ #include <avr/io.h>
 
-int main(void)
-{
-	DDRA = 0b11111111;
-    while (1) 
-    {
-		PORTA = 0b00000001;
-		_delay_ms(2000);
-		PORTA = 0b00000010;
-		_delay_ms(2000);
-		PORTA = 0b00000000;
-		_delay_ms(2000);
-    }
-	return 0;
-}
+ #include <util/delay.h>
+
+ //Simple Wait Function
+ void Wait()
+ {
+	 uint8_t i;
+	 for(i=0;i<50;i++)
+	 {
+		 _delay_loop_2(0);
+		 _delay_loop_2(0);
+		 _delay_loop_2(0);
+	 }
+
+ }
+
+ int main(void)
+ {
+	 //Configure TIMER1
+	 TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
+	 TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
+
+	 ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
+
+	 DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Out
+
+	 while(1)
+	 {
+
+		 OCR1A=97;   //0 degree
+		 Wait();
+
+		 OCR1A=316;  //90 degree
+		 Wait();
+
+		 OCR1A=425;  //135 degree
+
+		 Wait();
+
+		 OCR1A=535;  //180 degree
+		 Wait();
+	 }
+	 return 0;
+ }
+
 
