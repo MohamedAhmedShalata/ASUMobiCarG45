549bbe7f7110ff61656cdd8a7beadf029abd8e5a
MohamedHamedOo <36466919+MohamedHamedOo@users.noreply.github.com>
2018-04-09 10:02:55 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,69 @@
 /*
- * ServoMotor.c
+ * line follower with 3 sensors.c
  *
- * Created: 10/03/2018 11:10:58 م
+ * Created: 09/04/2018 09:16:05 ص
  * Author : MohameD AhmeD
  */ 
 
- #include <avr/io.h>
+ #define F_CPU 16000000UL
+  #include <avr/io.h>
 
- #include <util/delay.h>
-
- //Simple Wait Function
- void Wait()
+ void controlMotor (char m)
  {
-	 uint8_t i;
-	 for(i=0;i<50;i++)
+  
+
+    if (m=='F')
+    {
+	PORTC=0b00001010;
+    }
+	 if (m=='L')
 	 {
-		 _delay_loop_2(0);
-		 _delay_loop_2(0);
-		 _delay_loop_2(0);
+	PORTC=0b00001001;
 	 }
-
+	 if (m=='R')
+	 {
+	 PORTC=0b00000110;
+ 	 }
+	 
  }
-
- int main(void)
+ int main (void)
  {
-	 //Configure TIMER1
-	 TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
-	 TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
 
-	 ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
+ 
+ DDRB =0x00;
+  DDRC=0xff;
+   char F,L,R;
 
-	 DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Out
+ while (1)
+ {
+	 // pB0 for right sensor
+	 // pB1 for middle sensor
+	 // pB2 for left sensor
 
-	 while(1)
-	 {
-
-		 OCR1A=97;   //0 degree
-		 Wait();
-
-		 OCR1A=316;  //90 degree
-		 Wait();
-
-		 OCR1A=425;  //135 degree
-
-		 Wait();
-
-		 OCR1A=535;  //180 degree
-		 Wait();
+	 if ((PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && (PINB &(1<<PB2))){
+		 controlMotor(F);
 	 }
-	 return 0;
+	 if (!(PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && !(PINB &(1<<PB2))){
+		 controlMotor(F);
+	 }
+	 if ((PINB &(1<<PB0)) && (PINB &(1<<PB1)) && (PINB &(1<<PB2))){
+		 controlMotor(F);
+	 }
+	 if ((PINB &(1<<PB0)) && (PINB &(1<<PB1)) && !(PINB &(1<<PB2)))
+	 {
+		 controlMotor(L);
+	 }
+	 if ((PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && !(PINB &(1<<PB2)))
+	 {
+		 controlMotor(L);
+	 }
+	 if (!(PINB &(1<<PB0)) && (PINB &(1<<PB1)) && (PINB &(1<<PB2)))
+	 {
+		 controlMotor(R);
+	 }
+	 if (!(PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && (PINB &(1<<PB2)))
+	 {
+		 controlMotor(R);
+	 }
  }
-
-
+ }
