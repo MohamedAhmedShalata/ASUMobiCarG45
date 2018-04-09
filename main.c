e269f89cd8d4dd3b1e50d626203751c044e1b7c5
MohamedHamedOo <36466919+MohamedHamedOo@users.noreply.github.com>
2018-04-09 10:16:32 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,69 +1,33 @@
 /*
- * line follower with 3 sensors.c
+ * pwm.c
  *
- * Created: 09/04/2018 09:16:05 ص
+ * Created: 09/04/2018 08:44:08 ص
  * Author : MohameD AhmeD
  */ 
 
- #define F_CPU 16000000UL
-  #include <avr/io.h>
-
- void controlMotor (char m)
+ #include <avr/io.h>
+ #include <util/delay.h>
+ #define F_CPU1000000LU
+ int main(void)
  {
-  
-
-    if (m=='F')
-    {
-	PORTC=0b00001010;
-    }
-	 if (m=='L')
+	 DDRB|=(1<<PB2)|(1<<PB1);
+	 TCCR1A|=(1<<COM1A0)|(1<<COM1B0)|(1<<WGM10);
+	 TCCR1B|=(1<<CS10);
+	 while (1)
 	 {
-	PORTC=0b00001001;
-	 }
-	 if (m=='R')
-	 {
-	 PORTC=0b00000110;
- 	 }
-	 
- }
- int main (void)
- {
-
- 
- DDRB =0x00;
-  DDRC=0xff;
-   char F,L,R;
-
- while (1)
- {
-	 // pB0 for right sensor
-	 // pB1 for middle sensor
-	 // pB2 for left sensor
-
-	 if ((PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && (PINB &(1<<PB2))){
-		 controlMotor(F);
-	 }
-	 if (!(PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && !(PINB &(1<<PB2))){
-		 controlMotor(F);
-	 }
-	 if ((PINB &(1<<PB0)) && (PINB &(1<<PB1)) && (PINB &(1<<PB2))){
-		 controlMotor(F);
-	 }
-	 if ((PINB &(1<<PB0)) && (PINB &(1<<PB1)) && !(PINB &(1<<PB2)))
-	 {
-		 controlMotor(L);
-	 }
-	 if ((PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && !(PINB &(1<<PB2)))
-	 {
-		 controlMotor(L);
-	 }
-	 if (!(PINB &(1<<PB0)) && (PINB &(1<<PB1)) && (PINB &(1<<PB2)))
-	 {
-		 controlMotor(R);
-	 }
-	 if (!(PINB &(1<<PB0)) && !(PINB &(1<<PB1)) && (PINB &(1<<PB2)))
-	 {
-		 controlMotor(R);
+		 if (PINC&(1<<PC0))
+		 {
+			 OCR1A=60;
+		 }
+		 else if (PINC&(1<<PC1))
+		 {
+			 OCR1A=125;
+		 }
+		 else if (PINC=0)
+		 {
+			 OCR1A=255;
+		 }
+		 _delay_ms(10);
 	 }
  }
- }
+
